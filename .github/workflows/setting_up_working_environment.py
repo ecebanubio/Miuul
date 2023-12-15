dd6f0f50b06742d1726f17f628c27a9890beb62f
ecebanu <153176538+ecebanubio@users.noreply.github.com>
2023-12-15 13:30:18 +0300

    Add files via upload

---
+++ .github/workflows/setting_up_working_environment.py
@@ -0,0 +1,65 @@
+# sanal ortamlarin listelenmesi:
+# conda env list
+
+# conda create -n myenv
+# To activate this environment, use
+#
+#     $ conda activate myenv
+
+# To deactivate an active environment, use
+#
+#     $ conda deactivate
+
+# paket yukleme
+# conda install numpy
+
+# Ayni anda birden fazla paket yukleme
+# conda install numpy scipy pandas
+
+# Paket silme:
+# conda remove package_name
+
+# Belirli bir versiyona gore paket yukleme
+
+# conda install numpy=1.20.1
+
+# conda list
+
+# paket yukseltme icin upgrade:
+# conda upgrade conda
+
+# Tum paketlerin guncellenmesi yukseltilmesi:
+# conda upgrade -all
+
+# pip: pypi (python package index) paket yonetim araci
+
+# Paket yukleme:
+# pip install paket_adi
+# pip install pandas
+# Paket yukleme versiyona gore
+# pip install pandas==2.1.3
+
+# paketleri baska ortama tasima
+# conda env export > environment.yaml
+
+# sanal ortami silme
+# conda env remove -n myenv
+# Remove all packages in environment /Users/banu/anaconda3/envs/myenv:
+# conda env list
+# myenv listede olmayacak
+
+# paketleri bir yere cekme
+# conda env create -f environment.yaml
+
+# myenv ortami aktive etme komutu geldi
+# To activate this environment, use
+#
+#     $ conda activate myenv
+#
+# To deactivate an active environment, use
+#
+#     $ conda deactivate
+# varolan ortami yeni bir ortama tasiyoabiliriz
+
+# Sanal ortam olusturulup var olan programlar da yeni ortama tasindi
+
